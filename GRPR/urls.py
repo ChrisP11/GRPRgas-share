3d363fc19d03c881827e768c0e30cbfc126e0049
Chris Prouty <cprouty@Chriss-MacBook-Air.local>
2025-01-27 11:28:26 -0600

    fix admin overlap

--- GRPR/urls.py
+++ GRPR/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='GRPR/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='GRPR/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='GRPR/password_reset_complete.html'), name='password_reset_complete'),
-    path('admin/', views.admin_view, name='admin_page'),
+    path('admin_page/', views.admin_view, name='admin_page'),
     path('send-test-email/', views.send_test_email, name='send_test_email'),
     
 
